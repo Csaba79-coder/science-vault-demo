8c53f9ab5c5d6df4367cf59d2b6d639231eb07a0
Csaba Vadász <csabavadasz79@gmail.com>
2024-09-07 21:10:36 +0200

    Initial commit

---
+++ src/main/java/com/csaba79coder/sciencevaultdemo/ScienceVaultDemoApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.sciencevaultdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceVaultDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScienceVaultDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/sciencevaultdemo/ScienceVaultDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.sciencevaultdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceVaultDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
